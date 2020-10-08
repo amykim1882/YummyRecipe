0b621762adec2564748dee11af5b0f304b470ae4
tammieoh <oht@oxy.edu>
2020-10-07 23:11:26 -0700

    App.css + Filter
    Design the App.css plus input filter

--- src/components/recipes/Recipe.js
+++ src/components/recipes/Recipe.js
@@ -1,34 +1,36 @@
-import React, {Component} from 'react';
+import React, { Component } from "react";
+import "./Recipe.css";
 
-class Recipe extends Component{
+class Recipe extends Component {
+  constructor() {
+    super();
+    this.state = {};
+  }
+  // in my handle click
+  // set a new state with the opposite value of showVillager
 
-    constructor(){
-        super();
-        this.state ={
-        }
-    }
-    // in my handle click
-    // set a new state with the opposite value of showVillager
+  render() {
+    // console.log(this.props);
 
-
-    render(){
-        // console.log(this.props);
-        return(
-                <div className = "villager">
-                    <img src={this.props.recipe.image} 
-                    alt= "https://images.unsplash.com/photo-1498837167922-ddd27525d352?ixlib=rb-1.2.1&w=1000&q=80"/>
-                    <div>
-                        Title {this.props.recipe.title}
-                    </div>
-                    <div>
-                        Serving {this.props.recipe.servings}
-                    </div>
-                    <div>
-                        Calories {this.props.recipe.calories}
-                    </div>
-                </div>
-        );
-    }
+    return (
+      <div className="recipe_entry">
+        <img
+          src={this.props.recipe.image}
+          alt="https://images.unsplash.com/photo-1498837167922-ddd27525d352?ixlib=rb-1.2.1&w=1000&q=80"
+        />
+        <div id="Title">
+          <h2>{this.props.recipe.title}</h2>
+        </div>
+        <div id="Description">
+          <p>
+            Servings: {this.props.recipe.servings} <br></br>
+            Calories: {this.props.recipe.calories} <br></br>
+            Ratings: {this.props.recipe.rating}
+          </p>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default Recipe;
